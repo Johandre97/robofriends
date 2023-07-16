6c124ae758d8c02cab54878f7adc697ecc9d819e
Johandre97 <johandrehdb@gmail.com>
2023-07-16 16:32:39 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import { robots } from './robots';
+import SearchBox from './SearchBox.js'
 
-class App extends React.Component {
-  render() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots : robots,
+            searchfield : ''
+        }
+    }
+    render() {
+        return (
+            <div className="tc">
+                <h1 >RobotFriends</h1>
+                <SearchBox/>
+                <CardList robots={ robots }/>
+            </div>
+        )
+    }
 }
 
 export default App;
